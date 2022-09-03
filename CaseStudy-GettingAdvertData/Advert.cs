4e709206c1fc7fbed2ddba99b71215ae364fe1fe
mhsn99 <mahsuntay99@gmail.com>
2022-09-03 12:07:57 +0300

    add adverts' information model struct

---
+++ CaseStudy-GettingAdvertData/Advert.cs
@@ -0,0 +1,11 @@
+﻿namespace CaseStudy_GettingAdvertData
+{
+    /// <summary>
+    /// Adverts' information model.
+    /// </summary>
+    class Advert
+    {
+        public string Title { get; set; }
+        public string Price { get; set; }
+    }
+}
